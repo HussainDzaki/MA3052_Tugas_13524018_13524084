10a1e0d82978c93e1af82d592900408554a91247
Muhammad Nafis Habibi <137036100+NafisKreatif@users.noreply.github.com>
2026-02-25 10:57:48 +0700

    feat: add validation and string parameter in graph traversal

--- src/main/java/ma3052/graph/GraphTraversal.java
+++ src/main/java/ma3052/graph/GraphTraversal.java
@@ -6,20 +6,30 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class GraphTraversal {
+    public static List<Node> traversalOrderBFS(Graph graph, String startNodeName) {
+        return traversalOrderBFS(graph, graph.getNode(startNodeName));
+    }
+
     public static List<Node> traversalOrderBFS(Graph graph, Node startNode) {
+        if (!graph.hasNode(startNode)) {
+            return null;
+        }
+
         ArrayList<Node> nodeOrder = new ArrayList<Node>(graph.size());
-        
+
         HashSet<Long> visitedNodes = new HashSet<>();
         LinkedList<Node> queue = new LinkedList<Node>();
         queue.add(startNode);
         while (!queue.isEmpty()) {
             Node currentNode = queue.pop();
-            if (visitedNodes.contains(currentNode.getNodeID())) continue;
+            if (visitedNodes.contains(currentNode.getNodeID()))
+                continue;
             visitedNodes.add(currentNode.getNodeID());
             nodeOrder.add(currentNode);
 
             for (Node nextNode : currentNode.getAdjacencyList()) {
-                if (visitedNodes.contains(nextNode.getNodeID())) continue;
+                if (visitedNodes.contains(nextNode.getNodeID()))
+                    continue;
                 queue.add(nextNode);
             }
         }
@@ -27,20 +37,30 @@ public class GraphTraversal {
         return nodeOrder;
     }
 
+    public static List<Node> traversalOrderDFS(Graph graph, String startNodeName) {
+        return traversalOrderDFS(graph, graph.getNode(startNodeName));
+    }
+
     public static List<Node> traversalOrderDFS(Graph graph, Node startNode) {
+        if (!graph.hasNode(startNode)) {
+            return null;
+        }
+
         ArrayList<Node> nodeOrder = new ArrayList<Node>(graph.size());
-        
+
         HashSet<Long> visitedNodes = new HashSet<>();
         LinkedList<Node> queue = new LinkedList<Node>();
         queue.push(startNode);
         while (!queue.isEmpty()) {
             Node currentNode = queue.pop();
-            if (visitedNodes.contains(currentNode.getNodeID())) continue;
+            if (visitedNodes.contains(currentNode.getNodeID()))
+                continue;
             visitedNodes.add(currentNode.getNodeID());
             nodeOrder.add(currentNode);
 
             for (Node nextNode : currentNode.getAdjacencyList()) {
-                if (visitedNodes.contains(nextNode.getNodeID())) continue;
+                if (visitedNodes.contains(nextNode.getNodeID()))
+                    continue;
                 queue.push(nextNode);
             }
         }
