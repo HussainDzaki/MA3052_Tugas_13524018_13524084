993d526bcd7a155b9772732d872bede34f224028
Muhammad Nafis Habibi <137036100+NafisKreatif@users.noreply.github.com>
2026-03-18 21:59:25 +0700

    chore: make a todo for undo/redo

---
+++ src/main/java/ma3052/gui/UndoRedoInput.java
@@ -0,0 +1,12 @@
+package ma3052.gui;
+
+public class UndoRedoInput {
+    /*
+     * TODO: bikin undo redo pake ctrl + z dan (ctrl + y atau ctrl + shift + z)
+     * 
+     * Bikin tipe data untuk ngetrack perubahan (mirip-mirip commit di github),
+     * misal Changes
+     * Bikin Stack<Changes>
+     * Tinggal undo/redo yehehe
+     */
+}
