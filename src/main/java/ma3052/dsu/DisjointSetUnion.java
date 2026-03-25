3633692c4d580fd75bbc85ab98bef7cf9e4f5c6c
Muhammad Nafis Habibi <137036100+NafisKreatif@users.noreply.github.com>
2026-03-25 22:46:40 +0700

    feat: Disjoint Set Union for Kruskal

---
+++ src/main/java/ma3052/dsu/DisjointSetUnion.java
@@ -0,0 +1,35 @@
+package ma3052.dsu;
+
+import java.util.HashMap;
+import java.util.Random;
+
+public class DisjointSetUnion<T> {
+    private HashMap<T, T> dsuParent;
+    private Random rng = new Random();
+
+    public T getParent(T a) {
+        if (dsuParent.containsKey(a)) {
+            T parentA = getParent(dsuParent.get(a));
+            dsuParent.put(a, parentA);
+            return parentA;
+        } else {
+            return a;
+        }
+    }
+
+    public void unite(T a, T b) {
+        if (isSameSet(a, b)) {
+            return;
+        }
+        if (rng.nextBoolean()) {
+            dsuParent.put(getParent(a), getParent(b));
+        } else {
+            dsuParent.put(getParent(b), getParent(a));
+        }
+    }
+
+    public boolean isSameSet(T a, T b) {
+        return getParent(a) == getParent(b);
+    }
+
+}
